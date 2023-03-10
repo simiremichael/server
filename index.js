fb5c96da59d481c01a259b2dbb7b2e0268e7d011
Michael Simire <simiremichael@gmail.com>
2023-03-10 20:47:26 +0100

    updated

--- index.js
+++ index.js
@@ -13,15 +13,18 @@ const app = express();
 
 dotenv.config();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cookieParser())
  app.use(cors());
 
- res.setHeader('Access-Control-Allow-Origin', '*');
- res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
- res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
- 
+
 app.use(cors({
   credentials: true,
 }));
