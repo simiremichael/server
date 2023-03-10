fb07b7a0577d446d1b56e47b56714fd7389d1c37
Michael Simire <simiremichael@gmail.com>
2023-03-10 21:09:14 +0100

    updated

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cookieParser())
 
  app.use(cors({
   
-  origin: ['http://localhost:5000', 'https://server-beige-nine.vercel.app']
+  origin: ['http://localhost:3000', 'https://server-beige-nine.vercel.app']
 }))
 
 app.use('/properties', propertyRoute);
