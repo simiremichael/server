a7262f05637e3a5bc6e6f244d45eb02847b5a2bb
Michael Simire <simiremichael@gmail.com>
2023-03-11 20:07:24 +0100

    updated

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cookieParser())
 app.use((req, res, next) => {
   res.header({"Access-Control-Allow-Origin": "https://my-property-finder.vercel.app"});
   res.header({"Access-Control-Allow-Credentials": "true" });
-  res.header({"Content-Type": ["text/plain", "application/json"]});
+ // res.header({"Content-Type": ["text/plain", "application/json"]});
     next();
 }) 
 
