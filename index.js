ad8fc274954537efb3afa588878f1f80f29a523c
Michael Simire <simiremichael@gmail.com>
2023-03-11 20:08:21 +0100

    updated

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   res.header({"Access-Control-Allow-Origin": "https://my-property-finder.vercel.app"});
   res.header({"Access-Control-Allow-Credentials": "true" });
  // res.header({"Content-Type": ["text/plain", "application/json"]});
-    next();
+    //next();
 }) 
 
 
