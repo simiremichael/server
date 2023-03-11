ac02ff522bf3b22b7e9dec203048b392174bb3c1
Michael Simire <simiremichael@gmail.com>
2023-03-11 17:16:45 +0100

    updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cookieParser())
- app.use(cors());
+ //app.use(cors());
 
 
 // app.use((req, res, next) => {
