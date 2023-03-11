c4830f06b384dc49f31dfa821d26bcc303fd61e9
Michael Simire <simiremichael@gmail.com>
2023-03-11 12:44:55 +0100

    updated

--- index.js
+++ index.js
@@ -13,12 +13,6 @@ const app = express();
 
 dotenv.config();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cookieParser())
@@ -29,7 +23,7 @@ app.use(cookieParser())
 // origin: ['http://localhost:3000',
 // "https://server-beige-nine.vercel.app"]
 app.use((req, res, next) => {
-  res.header({"Access-Control-Allow-Origin": "*"});
+  res.header({"Access-Control-Allow-Origin": "http://localhost:3000"});
   next();
 }) 
 
