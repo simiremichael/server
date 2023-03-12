3d3d3c7b2b03b31655d543dfc357e08a76400e58
Michael Simire <simiremichael@gmail.com>
2023-03-12 19:13:12 +0100

    updated

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -22,7 +22,7 @@ export const signin = async (req, res) => {
    
          
         res.cookie(process.env.REACT_APP_AGENT_COOKIE_KEY, refreshToken, 
-        { httpOnly: true, secure: true, sameSite: 'lax', 
+        { httpsOnly: true, secure: true, sameSite: 'none', 
         maxAge: 7 * 24 * 60 * 60 * 1000 });
         res.status(200).json({ result: existingAgent, agentToken });
     } catch(error) {
