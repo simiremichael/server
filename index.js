58179c41e7edca437a743b185eae4966a6cd10e0
Michael Simire <simiremichael@gmail.com>
2023-03-12 18:20:44 +0100

    updated

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ app.use((req, res, next) => {
   res.set("Access-Control-Allow-Origin", "https://my-property-finder.vercel.app");
   res.set("Access-Control-Allow-Credentials", "true" );
   res.header("Access-Control-Allow-Origin", "https://my-property-finder.vercel.app");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
     next();
 }) 
 
